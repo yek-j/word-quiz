25165db2777dc7e31598e9688f2a7cac2f666630
yek-j <jeonyk219@gmail.com>
2025-03-14 20:28:54 +0900

    first commit

---
+++ src/main/java/com/jyk/wordquiz/wordquiz/WordQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.jyk.wordquiz.wordquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordQuizAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jyk/wordquiz/wordquiz/WordQuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyk.wordquiz.wordquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordQuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
